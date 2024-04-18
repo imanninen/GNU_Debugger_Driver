a1f4cd6179c62b054eb425ca9589872ccafb9f43
imanninen <igor.manninen@gmail.com>
2024-04-19 01:02:01 +0300

    clean up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun main() {
     debuggerDriver.loadDebugExecutable("debugee/target")
     debuggerDriver.setBreakPoint("main.c", 5)
     debuggerDriver.setBreakPoint("main.c", 12)
-    debuggerDriver.setBreakPointHandler {input, output ->
+    debuggerDriver.setBreakPointHandler { input, output ->
         println(debuggerDriver.getBackTrace(input, output))
         println(debuggerDriver.getVarValueByName("x", input, output))
         debuggerDriver.resume(input, output)
--- src/main/kotlin/config/lldb/commands/LldbPrintVarCommand.kt
+++ src/main/kotlin/config/lldb/commands/LldbPrintVarCommand.kt
@@ -3,17 +3,25 @@ package org.gnudebugger.config.lldb.commands
 import org.gnudebugger.config.core.commands.PrintVarCommand
 import org.gnudebugger.config.lldb.LldbDebugCommand
 import org.gnudebugger.config.lldb.responce.CommandResponse
+import org.gnudebugger.config.lldb.responce.ErrorCommandResponse
 import org.gnudebugger.config.lldb.responce.SuccessCommandResponse
 import java.io.BufferedReader
 
 internal class LldbPrintVarCommand(override val varName: String) : PrintVarCommand, LldbDebugCommand {
     override val clCommand: String
         get() = "print $varName\n"
+
     override fun handle(input: BufferedReader): CommandResponse {
         var outputOfCommand = ""
         input.skip(clCommand.length.toLong() + "(lldb) ".length.toLong())
         var line: String = input.readLine()
-        outputOfCommand += line
+        outputOfCommand += line.trimIndent()
+        if (outputOfCommand.contains(Regex("error: <user expression [0-9]*>:[0-9]*:[0-9]*:"))) {
+            val newOutput = outputOfCommand.replace(
+                Regex("error: <user expression [0-9]*>:[0-9]*:[0-9]*:"),
+                "ERROR:")
+            return ErrorCommandResponse(newOutput)
+        }
         return SuccessCommandResponse(outputOfCommand)
     }
 }
